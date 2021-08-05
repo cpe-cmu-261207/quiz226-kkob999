b4edac71585c486a18bf1c15757e7bcc31d9586c
koob <kob.ruji@gamil.com>
2021-08-05 14:25:22 +0700

    commit1

--- script.js
+++ script.js
@@ -3,11 +3,40 @@ const calculation = document.querySelector("#calculation")
 const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
+const reset_btn = document.querySelector('#reset')
+const search_btn = document.querySelector('#search')
+const text = document.querySelector('#text')
+var original = text.innerHTML
 
 // define more constants and variables here
 
 btn_toggle.onclick = () => {
-  // your code here
+  if (btn_toggle.innerHTML == "Show Calculation") {
+    const sum = parseInt('630610725') + parseInt(length.value)
+    author.innerHTML = sum;
+    btn_toggle.innerHTML = 'Show Author'
+  } else{
+    btn_toggle.innerHTML = 'Show Calculation'
+    author.innerHTML = '630610725 Chommanee Rujijanakul';
+  }
 }
 
+
 // more codes for Search and Reset buttons here
+let word = []
+search_btn.onclick = () => {
+  let newPara = ''
+  word = original.split(' ');
+  for (let i = 0; i < word.length; i++) {
+    if (word[i].length > length.value) {
+      newPara += "<span style = ' color: " + color.value + "'>" + word[i] + "</span>" + " "
+    }
+    
+  }
+}
+
+reset_btn.onclick = () => {
+  color.value = "#FF0000"
+  length.value = 5
+  text.innerHTML = original
+}
